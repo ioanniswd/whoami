57647b455d25acfbe6ae358a6437850ec3155d54
ioanniswd <ioanniswd@gmail.com>
2018-01-01 13:08:54 +0200

    Added favicon, fixed wrong ip
     On branch master
    
     Changes to be committed:
    	new file:   .gitignore
    	modified:   package.json
    	modified:   server.js

--- server.js
+++ server.js
@@ -1,21 +1,32 @@
+"use strict";
+
+var favicon = require('serve-favicon');
+var path = require('path');
 var express = require("express");
 var app = express();
 
 app.get('/', function(req, res) {
-    
-    var ip = req.headers["x-forwarded-for"];
-    var lang= req.headers["accept-language"];
-    var os = req.headers["user-agent"];
-    
-    lang = lang.match(/^[^,]*/);
-    os = os.match(/\(([^)]+)\)/);
-    
-    var object = {"ipaddress": ip, "language": lang[0], "software": os[0].slice(1,-1)};
-    
-   // res.send(req.headers);
-    res.send(object);
+
+  console.log('headers:', req.headers);
+
+  var ip = req.headers["x-forwarded-for"];
+  var lang = req.headers["accept-language"];
+  var os = req.headers["user-agent"];
+
+  lang = lang.match(/^[^,]*/);
+  os = os.match(/\(([^)]+)\)/);
+
+  var object = {
+    "ipaddress": ip.slice(0, ip.indexOf(',')),
+    "language": lang[0],
+    "software": os[0].slice(1, -1)
+  };
+  console.log('object:', object);
+
+  // res.send(req.headers);
+  res.send(object);
 });
 
 app.listen(8080, function() {
-   console.log('Listening on port 8080'); 
+  console.log('Listening on port 8080');
 });
